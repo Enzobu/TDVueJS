9c56d8aff13fd8438a70b28d2d0d23036f939552
jesuisleyanis <capelleyanis@gmail.com>
2023-12-08 19:17:14 +0100

    page TextImageComponent

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
    <NavigationComponent/>
+   <TextImageComponent/>
 </template>
 
 <script>
 
 import NavigationComponent from './components/NavigationComponent.vue';
+import TextImageComponent from './components/TextImageComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    NavigationComponent
+    NavigationComponent,
+    TextImageComponent,
   }
 }
 </script>
@@ -22,5 +25,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100%;
 }
+
+
 </style>
