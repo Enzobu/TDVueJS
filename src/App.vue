fa73a8980acf60e216ca33f2b17a825bea18c503
Yanis Capelle <capelleyanis@gmail.com>
2023-12-12 12:22:22 +0100

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
    <NavigationComponent/>
+   <FooterComponents/>
 </template>
 
 <script>
 
 // import FooterComponents from './components/FooterComponents.vue';
+import FooterComponents from './components/FooterComponents.vue';
 import NavigationComponent from './components/NavigationComponent.vue';
 
 export default {
   name: 'App',
   components: {
     NavigationComponent,
-  }
+    FooterComponents
+}
 }
 </script>
 
