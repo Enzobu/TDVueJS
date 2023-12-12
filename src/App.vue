942028f8df70630b77bbe0e15d754873637da147
Enzobu <enzo.palermo@epsi.fr>
2023-12-12 12:20:29 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
    <NavigationComponent/>
-   <TextImageComponent/>
 </template>
 
 <script>
 
-import FooterComponents from './components/FooterComponents.vue';
+// import FooterComponents from './components/FooterComponents.vue';
 import NavigationComponent from './components/NavigationComponent.vue';
 
 export default {
   name: 'App',
   components: {
     NavigationComponent,
-    TextImageComponent,
   }
 }
 </script>
