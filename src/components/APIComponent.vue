4e696841168bccd3bf9198bac65d745c025fa1a0
Enzobu <enzo.palermo@epsi.fr>
2023-12-12 15:31:26 +0100

    Update APIComponent.vue

--- src/components/APIComponent.vue
+++ src/components/APIComponent.vue
@@ -16,7 +16,8 @@
                     </p>
                     <p>
                         <label for="parameters">Paramètres</label>
-                        <input type="text" id="parameters" name="parameters" class="form-control" v-model="formData.parameters">
+                        <input type="text" id="parameters" name="parameters" class="form-control"
+                            v-model="formData.parameters">
                     </p>
                 </div>
                 <button class="btn btn-primary mt-2" type="submit">Valider</button>
@@ -25,62 +26,68 @@
     </div>
     <div class="row justify-content-center d-flex my-5 api-response mb-5" v-if="apiResponse">
         <div class="col-5">
-            <div class="row mb-4">Résultat :</div>
-            <div class="row border border-dark-subtle rounded p-1 overflow-y-scroll response mb-5"><pre>{{ formattedResponse }}</pre></div>
+            <div class="row mb-6">Résultat :</div>
+            <pre class="row border border-dark-subtle rounded p-1 response mb-5">{{ formattedResponse }}</pre>
         </div>
     </div>
 </template>
 
-
 <script>
+import axios from 'axios';
+
 export default {
     data() {
         return {
             formData: {
-                url: '',
                 method: 'GET',
-                parameters: '',
+                url: '',
+                parameters: {
+                    siret: 79317749400028,
+                }
+            }, computed: {
+                formattedResponse() {
+                    // Formatez la réponse de l'API pour l'afficher de manière lisible
+                    return JSON.stringify(this.apiResponse, null, 2);
+                },
             },
-            apiResponse: null,
+            apiResponse: null
         };
     },
-    computed: {
-    formattedResponse() {
-        // Formatez la réponse de l'API pour l'afficher de manière lisible
-        return JSON.stringify(this.apiResponse, null, 2);
-        },
-    },
     methods: {
-        async submitForm() {
-            try {
-                const response = await fetch(this.formData.url, {
-                    method: this.formData.method,
-                    headers: {
-                        'Content-Type': 'application/json',
-                        // Ajoutez d'autres en-têtes si nécessaire
-                    },
-                    // Ajoutez le corps de la requête pour les méthodes autres que GET
-                    body: this.formData.method === 'GET' ? null : JSON.stringify({}),
-                });
-
-                const data = await response.json();
-                this.apiResponse = data;
-            } catch (error) {
-                console.error('Erreur lors de la requête API :', error);
-                this.apiResponse = `L'API n'a rien retournée. (Erreur : ${error}). Vérifiez vos champs`;
-            }
+        submitForm() {
+            this.fetchData();
         },
+        fetchData() {
+            axios({
+                method: this.formData.method,
+                url: this.formData.url,
+                data: {
+                    siret: 79317749400028,
+                },
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => {
+                    this.apiResponse = response.data;
+                })
+                .catch(error => {
+                    this.apiResponse = `L'API n'a rien retourné. (Erreur : ${error}). Vérifiez vos champs`;
+                });
+        }
     },
+    mounted() {
+        // Vous pouvez appeler fetchData ici si vous voulez effectuer la requête au moment du montage
+    }
 };
 </script>
 
 <style>
 .api-response {
     max-height: 50vh;
-    
+
     & .response {
         max-height: 50vh;
     }
 }
-
 </style>
