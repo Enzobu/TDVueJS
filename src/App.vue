db8e24a5a603d1d3bc999a04cdf9545f29add464
Yanis Capelle <capelleyanis@gmail.com>
2023-12-12 11:53:15 +0100

    footer

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
    <NavigationComponent/>
    <TextImageComponent/>
+   <FooterComponents/>
 </template>
 
 <script>
 
+import FooterComponents from './components/FooterComponents.vue';
 import NavigationComponent from './components/NavigationComponent.vue';
 import TextImageComponent from './components/TextImageComponent.vue';
 
@@ -14,7 +16,8 @@ export default {
   components: {
     NavigationComponent,
     TextImageComponent,
-  }
+    FooterComponents
+}
 }
 </script>
 
@@ -27,6 +30,4 @@ export default {
   color: #2c3e50;
   width: 100%;
 }
-
-
 </style>
---
+++ src/components/FooterComponents.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="footer row mt-5 d-flex align-items-center">
+        <p class="mt-3 text-center">Créé par Yanis Capelle et Enzo Palermo </p>
+    </div>
+</template>
+
+<style>
+    .footer {
+        background-color: rgb(231, 231, 231);
+    }
+</style>
