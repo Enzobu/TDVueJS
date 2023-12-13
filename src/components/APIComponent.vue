ff3e69c134f161f4b7c9ef949dce7f8ae3dbdfe7
Yanis Capelle <capelleyanis@gmail.com>
2023-12-13 09:37:44 +0100

    update sizing form

--- src/components/APIComponent.vue
+++ src/components/APIComponent.vue
@@ -1,34 +1,36 @@
 <template>
-    <div class="row justify-content-center d-flex my-4">
-        <div class="col-sm-5">
-            <form @submit.prevent="submitForm" class="form-group mt-4">
-                <div id="school_fields">
-                    <p>
-                        <label for="url">URL :</label>
-                        <input type="text" id="url" name="url" class="form-control" v-model="formData.url" required>
-                    </p>
-                    <p>
-                        <label for="method">Méthode :</label>
-                        <select v-model="formData.method" id="method" class="form-select" required>
-                            <option value="GET">GET</option>
-                            <option value="POST">POST</option>
-                        </select>
-                    </p>
-                    <p>
-                        <label for="parameters">Paramètres</label>
-                        <input type="text" id="parameters" name="parameters" class="form-control"
-                            v-model="formData.parameters">
-                    </p>
-                </div>
-                <button class="btn btn-primary mt-2" type="submit">Valider</button>
-            </form>
+    <div class="mt-5">
+        <div class="row justify-content-center">
+            <div class="col-sm-5">
+                <form @submit.prevent="submitForm" class="form-group mt-4">
+                    <div id="school_fields">
+                        <div>
+                            <label class="form-label" for="url">URL :</label>
+                            <input type="text" id="url" name="url" class="form-control" v-model="formData.url" required>
+                        </div>
+                        <div class="my-3">
+                            <label class="form-label" for="method">Méthode :</label>
+                            <select v-model="formData.method" id="method" class="form-select" required>
+                                <option value="GET">GET</option>
+                                <option value="POST">POST</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label class="form-label" for="parameters">Paramètres</label>
+                            <input type="text" id="parameters" name="parameters" class="form-control"
+                                v-model="formData.parameters">
+                        </div>
+                    </div>
+                    <button class="btn btn-primary mt-4" type="submit">Valider</button>
+                </form>
+            </div>
         </div>
-    </div>
-    <div class="row justify-content-center d-flex my-5 api-response mb-5" v-if="apiResponse">
-        <div class="col-5">
-            <div class="row mb-6">Résultat :</div>
-            <pre class="row border border-dark-subtle rounded p-1 response mb-5">{{ formattedResponse }}</pre>
-        </div>
+        <div class="row justify-content-center d-flex my-5 api-response mb-5" v-if="apiResponse">
+            <div class="col-5">
+                <div class="row mb-6">Résultat :</div>
+                <pre class="row border border-dark-subtle rounded p-1 response mb-5">{{ formattedResponse }}</pre>
+            </div>
+        </div>  
     </div>
 </template>
 
